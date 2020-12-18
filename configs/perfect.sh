e7051f33c2e2fe55e8e728acf982e3ea53543e84
LucasS98 <lucasfernansalvador@outlook.com>
2020-12-17 23:44:56 -0300

    Fixed typos

--- configs/perfect.sh
+++ configs/perfect.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/cnfig_pascal_islip.icnt ./cnfig_pascal_islip.icnt
+cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/config_pascal_islip.icnt ./config_pascal_islip.icnt
 
 cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/gpgpusim.config ./gpgpusim.config
-./$1 > normal.txt
+./run > normal.txt
 
 cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/CU_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > CU_Perfect.txt
+./run > CU_Perfect.txt
 
 cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Exec_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Exec_Perfect.txt
+./run > Exec_Perfect.txt
 
 cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Mem_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Mem_Perfect.txt
+./run > Mem_Perfect.txt
 
 cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/OC_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > OC_Perfect.txt
+./run > OC_Perfect.txt
 
 cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/ICache_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > ICache_Perfect.txt
+./run > ICache_Perfect.txt
