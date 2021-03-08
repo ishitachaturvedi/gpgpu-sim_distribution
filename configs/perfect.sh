017f045f4672e70b09f5239dcaf7acf2933c20af
ishitachaturvedi <ishitac@princeton.edu>
2021-03-08 20:49:04 +0400

    Convert to lrr

--- configs/perfect.sh
+++ configs/perfect.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/config_pascal_islip.icnt ./config_pascal_islip.icnt
+cp -f /u/ishitac/gpgpusim-codes/perfect/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/config_pascal_islip.icnt ./config_pascal_islip.icnt
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/gpgpusim.config ./gpgpusim.config
+cp -f /u/ishitac/gpgpusim-codes/perfect/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/gpgpusim.config ./gpgpusim.config
 ./run > normal.txt
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/CU_Perfect/gpgpusim.config ./gpgpusim.config
+cp -f /u/ishitac/gpgpusim-codes/perfect/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/CU_Perfect/gpgpusim.config ./gpgpusim.config
 ./run > CU_Perfect.txt
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Exec_Perfect/gpgpusim.config ./gpgpusim.config
+cp -f /u/ishitac/gpgpusim-codes/perfect/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Exec_Perfect/gpgpusim.config ./gpgpusim.config
 ./run > Exec_Perfect.txt
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Mem_Perfect/gpgpusim.config ./gpgpusim.config
+cp -f /u/ishitac/gpgpusim-codes/perfect/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Mem_Perfect/gpgpusim.config ./gpgpusim.config
 ./run > Mem_Perfect.txt
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/OC_Perfect/gpgpusim.config ./gpgpusim.config
+cp -f /u/ishitac/gpgpusim-codes/perfect/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/OC_Perfect/gpgpusim.config ./gpgpusim.config
 ./run > OC_Perfect.txt
 
-cp -f /u/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/ICache_Perfect/gpgpusim.config ./gpgpusim.config
+cp -f /u/ishitac/gpgpusim-codes/perfect/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/ICache_Perfect/gpgpusim.config ./gpgpusim.config
 ./run > ICache_Perfect.txt
