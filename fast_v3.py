4ed6dacac32a54f112e2f1727332ab03c267d237
LucasS98 <lucasfernansalvador@outlook.com>
2021-04-05 00:33:59 -0400

    NewPath

--- fast_v3.py
+++ fast_v3.py
@@ -374,7 +374,7 @@ def profileStalls(filename, fixedStalls):
 
 # Main Function
 def main():
-    data_folder = Path("/u/ls24/rodinia/cuda/nn/")
+    data_folder = Path("/u/ishitac/gpu_codes/rodinia_3.1/cuda/nn")
     filename = data_folder / "stall_output.txt"
 
     fixedStalls = [Stall.Mem_data]
