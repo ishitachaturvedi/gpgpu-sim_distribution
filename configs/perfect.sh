4eb5ef4d170f46ce24601e89984b2ba6bddb815c
Lucas Salvador <ls24@princeton.edu>
2021-05-07 17:24:00 +0000

    Changed to 1 SM

--- configs/perfect.sh
+++ configs/perfect.sh
@@ -2,34 +2,36 @@
 
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/config_pascal_islip.icnt ./config_pascal_islip.icnt
 
+mkdir onesm
+
 echo NORMAL
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/gpgpusim.config ./gpgpusim.config
-./$1 > normal.txt
+./$1 > onesm/normal.txt
 
 echo EXEC_STRUCT
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Exec_Struct_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Exec_Struct_Perfect.txt
+./$1 > onesm/Exec_Struct_Perfect.txt
 
 echo EXEC
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Exec_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Exec_Perfect.txt
+./$1 > onesm/Exec_Perfect.txt
 
 echo MEM
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Mem_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Mem_Perfect.txt
+./$1 > onesm/Mem_Perfect.txt
 
 echo MEM_STRUCT
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Mem_Struct_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Mem_Struct_Perfect.txt
+./$1 > onesm/Mem_Struct_Perfect.txt
 
 echo ICACHE
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/ICache_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > ICache_Perfect.txt
+./$1 > onesm/ICache_Perfect.txt
 
 echo CONTROL
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Control_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Control_Perfect.txt
+./$1 > onesm/Control_Perfect.txt
 
 echo SYNCHRO
 cp -f /scratch/ls24/gpgpu-sim_distribution/configs/tested-cfgs/SM6_TITANX/Synchro_Perfect/gpgpusim.config ./gpgpusim.config
-./$1 > Synchro_Perfect.txt
+./$1 > onesm/Synchro_Perfect.txt
